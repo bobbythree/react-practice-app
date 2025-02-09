bb80a129569ea63db82977f65a77449534038630
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-08 19:44:49 -0600

    pass props to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-export default function Navbar() {
+export default function Navbar({logo="dudeman"}) {
   const links = [
     {id: 1, title: 'about', href: '/'},
     {id: 2, title: 'blog', href: '/'},
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <nav className=
     "flex justify-around p-5 bg-slate-200">
-      <h3 className="mr-100 font-bold">Dudeman</h3>
+      <h3 className="mr-100 font-bold">{logo}</h3>
       <div className="flex gap-15">
         {links.map((link) => (
           <a href={link.href} key={link.id}>{link.title}</a>
@@ -17,8 +17,8 @@ export default function Navbar() {
       <div className="dropdown dropdown-hover">
       <div tabIndex={0} role="button" className="btn mb-1 bg-red-800 text-slate-300">CTA</div>
         <ul tabIndex={0} className="dropdown-content menu bg-slate-600 rounded-box z-1 w-52 p-2 shadow-sm">
-          <li className="text-slate-200"><a>Item 1</a></li>
-          <li className="text-slate-200"><a>Item 2</a></li>
+          <li className="text-slate-200"><a>call to action 01</a></li>
+          <li className="text-slate-200"><a>call to action 02</a></li>
         </ul>
       </div>
       </div>
